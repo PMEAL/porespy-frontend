2153028c68ad17f518256fcb5c45966d533e47c8
espitiaandres <espitiaandres123@gmail.com>
2020-12-12 22:18:58 -0500

    added CORS to backend, django api now accessible from frontend

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 import './LandingPage.css';
 
 const LandingPage = () => {
+    const testBackend = () => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:8000/users/',
+            auth: {
+                username: 'admin',
+                password: 'porespyBackend'
+            }
+        }).then((response) => {
+            console.log("users have been fetched!");
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div>
             <div className="title">
@@ -10,6 +28,9 @@ const LandingPage = () => {
             <div className="description">
                 (Insert description here)
             </div>
+            <Button variant="contained" color="primary" onClick={() => testBackend()}>
+                Primary
+            </Button>            
         </div>
     )
 }
